988b16268b3770c6b4785e745809e4808d0f74c9
izik4004 <izik4004@gmail.com>
2022-05-14 02:32:59 +0100

    changes styling

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import "./hero.css"
-import img from "../../img/11124.jpg"
+import img from "../../img/2.png"
 const Hero = () => {
   return (
-    <div className=" hero-section h-screen flex justify-center align-middle items-center text-center">
+    <div className=" hero-section gap-10 bg-[#1e2230] mb-10">
+           <div className=' h-screen max-w-7xl  mx-auto flex justify-center align-middle items-center text-center'>
             <div className='max-w-3xl'>
-               <h1 className="flex font-bold text-7xl ">
+            <p className='py-10 text-left text-lg font-semibold text-[#fcb005]'>Crowdfounding Agency</p>
+               <h1 className="flex font-bold text-7xl text-white text-left">
                   Raising Hands to Promote Best Project for Funding
                </h1>
-               <p>Showcase your product and get funding</p>
-               <button className='btn'>Join</button>
+               <p className='text-2xl py-10 text-white text-left'>Showcase your product and get funding</p>
+               <div className='py-10 justify-start flex'>
+               <button className='btn px-10 py-4 text-lg bg-[#333CC1]'>Showcase your Startup</button>
+               </div>
             </div>
-            <div className='max-w-3xl'>
-                <img src={img} alt="" />
+            <div className='max-w-4xl'>
+                <img src={img} alt="" className="w-fit" />
+            </div>
             </div>
          </div>
   )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,10 +3,10 @@ import React from "react";
 
 const Navbar = () => {
    return (
-      <div className="navbar bg-[#333CC1] h-20">
-         <div className="container text-xl flex justify-between align-middle items-center text-center py-10 max-w-7xl mx-auto">
+      <div className="navbar bg-[#1e2230] h-20 py-5">
+         <div className="container text-xl flex justify-between align-middle items-center text-center py-2 max-w-7xl mx-auto">
             <div className="logo">Logo</div>
-            <ul className="nav-menu flex gap-5 text-white">
+            <ul className="nav-menu flex gap-5 text-white font-bold">
                <li>Home</li>
                <li>Order</li>
                <li>Menu</li>
