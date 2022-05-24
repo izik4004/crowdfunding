cce38b5bb0b8ff214f43a16c7532ae0f89b3fc75
izik4004 <izik4004@gmail.com>
2022-05-24 18:09:43 +0100

    connecting metamask

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,38 @@
-import React from "react";
+import {useState} from "react";
+import Welcome from "../Welcome";
 
 
 const Navbar = () => {
+   const [isConnected, setIsConnected] = useState(false)
+
+   const onLogin = () => {
+      setIsConnected(true);
+   }
+
+   // const onLogout = () => {
+   //    setIsConnected(false);
+   // }
+
+   const onLoginHandler =() => {
+      onLogin(false)
+   }
    return (
       <div className="navbar bg-[#1e2230] h-20 py-5">
          <div className="container text-xl flex justify-between align-middle items-center text-center py-2 max-w-7xl mx-auto">
             <div className="logo">Logo</div>
             <ul className="nav-menu flex gap-5 text-white font-bold">
-               <li>Home</li>
+               {/* <li>Home</li>
                <li>Order</li>
-               <li>Menu</li>
-               <li>Delivery</li>
+               <li>Menu</li> */}
+               <li>Products</li>
                <li>Contact</li>
             </ul>
-            <button className="btn">Sign Up</button>
+            {!isConnected &&
+            <button  onClick={onLoginHandler} className="btn">Connect Wallet</button>
+          
+            }
             {/* <div className="hamburger"></div> */}
+            {isConnected && <Welcome/>}
          </div>
 
          
